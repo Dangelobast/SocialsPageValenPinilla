54a8bbcf0227cf2fbcc069c50dfff333a517e68c
Dangelobast <dantidas1@gmail.com>
2025-08-12 23:10:19 -0500

    Socials Page Valen Pinilla, HTML, CSS, JAVASCRIPT

---
+++ javascript/script.js
@@ -0,0 +1,37 @@
+'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+function createHeart(){
+  const container = document.querySelector('.card-container');
+  const svg = document.querySelector('.heart-icon');
+  const svgClone = svg.cloneNode(true);
+  const personName = document.querySelector('.profile-name');
+  const audio = document.querySelector('.audio-file');
+
+  svgClone.style.left = Math.random()*100 + "%";
+  svgClone.style.animation = 'floatUp 8s';
+  
+  svgClone.style.width = Math.random() * 80 + 40 + 'px';
+  svgClone.style.cursor = "pointer";
+  container.appendChild(svgClone);
+  svgClone.addEventListener('click', ()=>{
+    audio.play();
+    svgClone.style.transition = "0.5s";
+    svgClone.style.width = "150px";
+    svgClone.querySelector('.heart-path').style.fill = "red";
+    personName.style.color = '#c7365f';
+   setTimeout(() => {
+    personName.style.transition = 'color 0.5s'
+    personName.style.color = 'rgba(255, 255, 255, 0.452)';
+  }, 300);
+
+
+    
+  });
+  svgClone.addEventListener('animationend', ()=>{
+    svgClone.remove();
+  })
+  
+}
+
+setInterval(createHeart, 1000);
+});

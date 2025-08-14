5e5836c650d9737d83b63a0d61f0cce83d4c51c0
Dangelobast <dantidas1@gmail.com>
2025-08-14 13:12:33 -0500

    fixing horizontal overflow, heart overflowing content causing scroll

--- javascript/script.js
+++ javascript/script.js
@@ -1,36 +1,38 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', () => {
-function createHeart(){
+function createHeart() {
   const container = document.querySelector('.card-container');
   const svg = document.querySelector('.heart-icon');
   const svgClone = svg.cloneNode(true);
   const personName = document.querySelector('.profile-name');
   const audio = document.querySelector('.audio-file');
 
-  svgClone.style.left = Math.random()*100 + "%";
+  const containerWidth = container.offsetWidth;
+  console.log(containerWidth);
+  const heartWidth = Math.random() * 80 + 40;
+  svgClone.style.width = heartWidth + 'px';
+  const maxLeft = containerWidth - heartWidth;
+  const left = Math.random() * maxLeft;
+  svgClone.style.left = left + 'px';
   svgClone.style.animation = 'floatUp 8s';
-  
-  svgClone.style.width = Math.random() * 80 + 40 + 'px';
   svgClone.style.cursor = "pointer";
   container.appendChild(svgClone);
-  svgClone.addEventListener('click', ()=>{
+
+  svgClone.addEventListener('click', () => {
     audio.play();
     svgClone.style.transition = "0.5s";
     svgClone.style.width = "150px";
     svgClone.querySelector('.heart-path').style.fill = "red";
     personName.style.color = '#c7365f';
-   setTimeout(() => {
-    personName.style.transition = 'color 0.5s'
-    personName.style.color = 'rgba(255, 255, 255, 0.452)';
-  }, 300);
-
-
-    
+    setTimeout(() => {
+      personName.style.transition = 'color 0.5s'
+      personName.style.color = 'rgba(255, 255, 255, 0.452)';
+    }, 300);
   });
-  svgClone.addEventListener('animationend', ()=>{
+
+  svgClone.addEventListener('animationend', () => {
     svgClone.remove();
-  })
-  
+  });
 }
 
 setInterval(createHeart, 1000);
